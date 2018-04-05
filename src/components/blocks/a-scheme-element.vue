0f2bb6bf17ae69b4ec9fbd3a7c3640f46297d8d2
garryk3 <egoruum@gmail.com>
2018-04-06 01:14:35 +0300

    develop

--- src/components/blocks/a-scheme-element.vue
+++ src/components/blocks/a-scheme-element.vue
@@ -1,18 +1,22 @@
 <template>
   <v-card class="a-scheme-element">
-    <v-card-title primary-title class="a-scheme-element__body">
-      <h3 class="title">Заполните атрибуты элемента</h3>
-      <v-text-field
-        name="name"
-        label="Название шаблона"
-        id="testing"
-        v-model="nameValue"
-      ></v-text-field>
-    </v-card-title>
-    <v-card-actions>
-      <v-btn @click="onCreateElement">Создать элемент</v-btn>
-      <v-btn @click="onResetElement">Отмена</v-btn>
-    </v-card-actions>
+    <v-form v-model="elementValid">
+      <v-card-title primary-title class="a-scheme-element__body">
+        <h3 class="title">Заполните атрибуты элемента</h3>
+        <v-text-field
+          name="name"
+          label="Название шаблона"
+          id="testing"
+          v-model="nameValue"
+          :rules="nameValueRules"
+          required
+        ></v-text-field>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn @click="onCreateElement">Создать элемент</v-btn>
+        <v-btn @click="onResetElement">Отмена</v-btn>
+      </v-card-actions>
+    </v-form>
   </v-card>
 </template>
 
@@ -22,7 +26,12 @@ export default {
   name: 'a-scheme-element',
   data() {
     return {
-      nameValue: ''
+      elementValid: false,
+      nameValue: '',
+      nameValueRules: [
+        v => !!v || 'Имя обязательно',
+        v => v.length > 1 || 'Не менее 2х символов'
+      ]
     };
   },
   methods: {
